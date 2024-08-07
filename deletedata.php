8d4525661c693d75e07d7d62b899621708c7c6cc
Mahnoor Tabassum <163853352+Mahnoor760@users.noreply.github.com>
2024-08-07 21:39:54 +0500

    Add files via upload
    PHP Project

---
+++ deletedata.php
@@ -0,0 +1,11 @@
+<?php
+    require_once "conn.php";
+    $id = $_GET["id"];
+    $query = "DELETE FROM student WHERE id = '$id'";
+    if (mysqli_query($conn, $query)) {   
+        header("location: index.php");
+       
+    } else {
+         echo "Something went wrong. Please try again later.";
+    }
+?>
